5af25ac4bb8ee82efad1433eac6c22d21bd9b0d2
Ariel Gonzalez <arielgonzalezr9@gmail.com>
2024-11-26 20:57:32 -0300

    Se agregó el diseño responsive

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -1,40 +1,38 @@
-    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-
-            <div class="carousel-item active">
-                <img src="assets/img/Mangas/akamegakill01.jpg" class="d-block w-30" alt="Akame Ga Kill Vol. 1">
-            </div>
-
-            <div class="carousel-item">
-                <img src="assets/img/Mangas/chainsawman01.jpg" class="d-block w-30" alt="Akame Ga Kill Vol. 2">
-            </div>
-
-            <div class="carousel-item">
-                <img src="assets/img/Mangas/deathnote01.jpg" class="d-block w-30" alt="Akame Ga Kill Vol. 3">
-            </div>
-
+<!-- Carrusel de imágenes -->
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/img/Mangas/akamegakill01.jpg" class="d-block img-fluid mx-auto" alt="Akame Ga Kill Vol. 1">
         </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-
+        <div class="carousel-item">
+            <img src="assets/img/Mangas/chainsawman01.jpg" class="d-block img-fluid mx-auto" alt="Akame Ga Kill Vol. 2">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/img/Mangas/deathnote01.jpg" class="d-block img-fluid mx-auto" alt="Akame Ga Kill Vol. 3">
+        </div>
     </div>
 
-    <div class="container">
-        <h1>Productos</h1>
-        <div class="product-list">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
 
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<!-- Lista de productos -->
+<div class="container">
+    <h1 class="text-center mt-4">Productos</h1>
+    <div class="row">
+        <!-- Producto 1 -->
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <div class="product-item">
-                <img src="assets/img/Comics/spiderman1.png" alt="Cómic">
+                <img src="assets/img/Comics/spiderman1.png" alt="Cómic" class="img-fluid">
                 <div class="product-details">
                     <h2 class="product-title">
                         <a href="comics.html">Cómics</a>
@@ -43,9 +41,11 @@
                         favoritos.</p>
                 </div>
             </div>
-
+        </div>
+        <!-- Producto 2 -->
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <div class="product-item">
-                <img src="assets/img/Mangas/deathnote01.jpg" alt="Manga">
+                <img src="assets/img/Mangas/deathnote01.jpg" alt="Manga" class="img-fluid">
                 <div class="product-details">
                     <h2 class="product-title">
                         <a href="mangas.html">Mangas</a>
@@ -53,20 +53,23 @@
                     <p class="product-description">Descubre los mejores mangas directamente desde Japón.</p>
                 </div>
             </div>
-
+        </div>
+        <!-- Producto 3 -->
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <div class="product-item">
-                <img src="assets/img/Figuras/figurassjtrunks.png" alt="Figura">
+                <img src="assets/img/Figuras/figurassjtrunks.png" alt="Figura" class="img-fluid">
                 <div class="product-details">
                     <h2 class="product-title">
                         <a href="figuras.html">Figuras</a>
                     </h2>
-                    <p class="product-description">Figuras coleccionables de alta calidad para todos los fanáticos.
-                    </p>
+                    <p class="product-description">Figuras coleccionables de alta calidad para todos los fanáticos.</p>
                 </div>
             </div>
-
+        </div>
+        <!-- Producto 4 -->
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <div class="product-item">
-                <img src="assets/img/Merch/remeraspiderman.png" alt="Ropa">
+                <img src="assets/img/Merch/remeraspiderman.png" alt="Ropa" class="img-fluid">
                 <div class="product-details">
                     <h2 class="product-title">
                         <a href="ropa.html">Merchandising</a>
@@ -76,39 +79,35 @@
             </div>
         </div>
     </div>
+</div>
 
-    <div class="contenedor-principal">
-        <h2 class="titulo">Últimos ingresos</h2>
-        <div class="contenedor-imagenes">
-            <!-- Imagen 1 -->
-            <div class="item">
-                <a href="https://www.example.com/imagen1" target="_blank">
-                    <img src="assets/img/Figuras/figurabrolyssj.png" alt="Imagen 1">
-                </a>
-            </div>
-            <!-- Imagen 2 -->
-            <div class="item">
-                <a href="https://www.example.com/imagen2" target="_blank">
-                    <img src="assets/img/Figuras/figuragokussj.png" alt="Imagen 2">
-                </a>
-            </div>
-            <!-- Imagen 3 -->
-            <div class="item">
-                <a href="https://www.example.com/imagen3" target="_blank">
-                    <img src="assets/img/Merch/remeraVenom.png" alt="Imagen 3">
-                </a>
-            </div>
-            <!-- Imagen 4 -->
-            <div class="item">
-                <a href="https://www.example.com/imagen4" target="_blank">
-                    <img src="assets/img/Merch/remerahellfireclub.png" alt="Imagen 4">
-                </a>
-            </div>
-            <!-- Imagen 5 -->
-            <div class="item">
-                <a href="https://www.example.com/imagen5" target="_blank">
-                    <img src="assets/img/Comics/batmanFlashpoint.png" alt="Imagen 5">
-                </a>
-            </div>
+<!-- Últimos ingresos -->
+<div class="contenedor-principal">
+    <h2 class="titulo">Últimos ingresos</h2>
+    <div class="row">
+        <!-- Imagen 1 -->
+        <div class="col-6 col-md-4 col-lg-3">
+            <a href="https://www.example.com/imagen1" target="_blank">
+                <img src="assets/img/Figuras/figurabrolyssj.png" alt="Imagen 1" class="img-fluid">
+            </a>
+        </div>
+        <!-- Imagen 2 -->
+        <div class="col-6 col-md-4 col-lg-3">
+            <a href="https://www.example.com/imagen2" target="_blank">
+                <img src="assets/img/Figuras/figuragokussj.png" alt="Imagen 2" class="img-fluid">
+            </a>
+        </div>
+        <!-- Imagen 3 -->
+        <div class="col-6 col-md-4 col-lg-3">
+            <a href="https://www.example.com/imagen3" target="_blank">
+                <img src="assets/img/Merch/remeraVenom.png" alt="Imagen 3" class="img-fluid">
+            </a>
+        </div>
+        <!-- Imagen 4 -->
+        <div class="col-6 col-md-4 col-lg-3">
+            <a href="https://www.example.com/imagen4" target="_blank">
+                <img src="assets/img/Merch/remerahellfireclub.png" alt="Imagen 4" class="img-fluid">
+            </a>
         </div>
     </div>
+</div>
